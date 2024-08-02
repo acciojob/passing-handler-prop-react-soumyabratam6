d741732be088aedd1c532c09b3f976dc9961fd26
soumyabrata <soumyabratam6@gmail.com>
2024-08-02 08:00:59 +0530

    all file added

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from 'react';
+
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({ background: '' });
+
+  return (
+    <div 
+      className="fix-box" 
+      style={{...style, height: '100px', width: '100px'}}
+      onClick={() => applyColor(setStyle)}
+    >
+    </div>
+  );
+};
+
+export default Selection;
 
